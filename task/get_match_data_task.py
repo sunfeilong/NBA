522b0984e342423fcb81ca42191b18211ca96ef7
sunfeilong <sunfeilong1993@gmail.com>
2019-05-10 13:26:20 +0800

    拉取数据任务增加异常处理

--- task/get_match_data_task.py
+++ task/get_match_data_task.py
@@ -20,14 +20,18 @@ class GetMatchDataTask(threading.Thread):
 
     def run(self):
         while not self.match_container.all_match_has_end() or self.fetch_times < 2:
-            self.fetch_times += 1
-            match_list = self.data_fetch.fetch_match_list(datetime.datetime.fromtimestamp(time.time()))
-            if not match_list:
-                print('no game!')
-            else:
-                for match in match_list:
-                    self.match_container.add_or_update_match(match)
-            time.sleep(max(5, self.delay))
+            try:
+                self.fetch_times += 1
+                match_list = self.data_fetch.fetch_match_list(datetime.datetime.fromtimestamp(time.time()))
+                if not match_list:
+                    print('no game!')
+                else:
+                    for match in match_list:
+                        self.match_container.add_or_update_match(match)
+            except BaseException as e:
+                self.logger.error('GetMatchDataTask run exception: {}'.format(e))
+            finally:
+                time.sleep(max(5, self.delay))
 
         self.logger.info('GetMatchDataTask has end, fetch times: [{}] task name: {}'
                          .format(self.fetch_times, threading.current_thread().name))
--- task/get_match_message_task.py
+++ task/get_match_message_task.py
@@ -18,10 +18,14 @@ class GetMatchMessageTask(threading.Thread):
 
     def run(self):
         while self.match_container.is_not_finished(self.match_id) or self.fetch_times < 2:
-            self.fetch_times += 1
-            message_list = self.data_fetch.fetch_message_list(self.match_id)
-            self.match_container.add_message(self.match_id, message_list)
-            self.logger.info('add message to {}, message size:{}'.format(self.match_id, len(message_list)))
-            time.sleep(max(5, self.delay))
+            try:
+                self.fetch_times += 1
+                message_list = self.data_fetch.fetch_message_list(self.match_id)
+                self.match_container.add_message(self.match_id, message_list)
+                self.logger.info('add message to {}, message size:{}'.format(self.match_id, len(message_list)))
+            except BaseException as e:
+                self.logger.error('GetMatchMessageTask run exception: {}'.format(e))
+            finally:
+                time.sleep(max(5, self.delay))
         self.logger.info('GetMatchMessageTask has end, fetch times [{}], task name:[{}]'
                          .format(self.fetch_times, threading.current_thread().name))
